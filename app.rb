9eb21d9dd0c13979dae08c210cef1cec87fbe89f
arnold bwaila <53196477+dlonra22@users.noreply.github.com>
2020-06-29 23:14:21 +0100

    passed all tests

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Arnold Bwaila"
+end
+get '/hometown' do
+  "My hometown is Gweru"
+end
+get '/favorite-song' do
+"My favorite song is Back That Ass Up"
+end
+
 end
